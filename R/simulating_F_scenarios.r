aac05a8c85117e2bf4ea451b5a001828ec115f13
MOshima-PIFSC <megumi.oshima@noaa.gov>
2024-05-01 16:24:31 -1000

    continue transitioning ss3sim model to opaka

--- R/simulating_F_scenarios.r
+++ R/simulating_F_scenarios.r
@@ -4,19 +4,19 @@ library(ss3sim)
 
 main.dir <- this.path::here(.. = 1)
 source(file.path(main.dir, "R", "simF.r"))
-dat <- SS_readdat_3.30(file = file.path(main.dir, "OM_sim_data", "data.ss"))
-dat$catch$catch <- 1
-head(dat$catch)
+# dat <- SS_readdat_3.30(file = file.path(main.dir, "OM_sim_data", "data.ss"))
+# dat$catch$catch <- 1
+# head(dat$catch)
 
-dat$CPUE$obs <- ifelse(dat$CPUE$index == 1, 1, dat$CPUE$obs)
-dat$CPUE$se_log <- ifelse(dat$CPUE$index == 1, 0.2, dat$CPUE$se_log)
-SS_writedat_3.30(dat, outfile = file.path(main.dir, "OM_sim_data", "data.ss"), overwrite = T)
+# dat$CPUE$obs <- ifelse(dat$CPUE$index == 1, 1, dat$CPUE$obs)
+# dat$CPUE$se_log <- ifelse(dat$CPUE$index == 1, 0.2, dat$CPUE$se_log)
+# SS_writedat_3.30(dat, outfile = file.path(main.dir, "OM_sim_data", "data.ss"), overwrite = T)
 
-dat <- SS_readdat_3.30(file = file.path(main.dir, "OM_sim_data", "data.ss_new"), section = 3)
-SS_writedat_3.30(dat, outfile = file.path(main.dir, "OM_sim_data", "with_sim_data", "data.ss"), overwrite = T)
-rep <- SS_output(dir = file.path(main.dir, "OM_sim_data"))
-rep <- SS_output(dir = file.path(main.dir, "OM_sim_data", "with_sim_data"))
-SS_plots(rep)
+# dat <- SS_readdat_3.30(file = file.path(main.dir, "OM_sim_data", "data.ss_new"), section = 3)
+# SS_writedat_3.30(dat, outfile = file.path(main.dir, "OM_sim_data", "with_sim_data", "data.ss"), overwrite = T)
+# rep <- SS_output(dir = file.path(main.dir, "OM_sim_data"))
+# rep <- SS_output(dir = file.path(main.dir, "OM_sim_data", "with_sim_data"))
+# SS_plots(rep)
 
 ## Create F timeseries with no variability
 scale = 0.1 /1.323 #scales how large F gets
@@ -121,7 +121,7 @@ SS_plots(rep2)
 ## create loop for steps 5-8
 scale = 0.1 /1.323
 Edat<-getFhist(nsim=10,Esd=0.001,nyears=100,dFmin=-0.12,dFmax=0.12,bb=0.1,scale=scale)
-# matplot(x=seq(1,100,1),t(Edat),type='l',ylim=c(0,0.2),xlab="",ylab="Fishing mortality", lwd = 3,cex.lab=1.5,cex.axis=1.5)
+# matplot(x=seq(1,100,1),t(Edat),type='l',ylim=c(0,0.2),xlab="",ylab="Fishing mortality", lwd = 3,cex.lab=1.5,cex.axis=1.5,col=1:10)
 # D<-round(mean(apply(Edat,1,mean)),2)
 # legend('topleft',legend=paste("Mean F=",D,sep=""),bty='n')
 # legend("topright", legend = paste0("Effort series ", seq(1,10)), lty = 1:10, col = 1:10, lwd = 2)
@@ -195,4 +195,102 @@ scale_color_manual(values = cols[c(8,10,6)], labels = c("Decreasing E", "Flat E"
 labs(x = "Year", y = "Index", color = "")  +
 theme(text = element_text(size = 16))
 
+## Visualizing different levels of inter-annual variability
+Edat_var.01<-getFhist(nsim=10,Esd=0.01,nyears=100,dFmin=-0.12,dFmax=0.12,bb=0.1,scale=scale)
+Edat_var.05<-getFhist(nsim=10,Esd=0.05,nyears=100,dFmin=-0.12,dFmax=0.12,bb=0.1,scale=scale)
+Edat_var.1<-getFhist(nsim=10,Esd=0.1,nyears=100,dFmin=-0.12,dFmax=0.12,bb=0.1,scale=scale)
 
+par(mfrow=c(1,3),mar=c(4,5,0.5,0.5), oma=c(1.5,1.5,0.5,0.5),cex=1.2)
+matplot(x=seq(1,100,1),t(Edat_var.01),type='l',ylim=c(0,0.2),xlab="",ylab="Fishing mortality", lwd = 3,cex.lab=1.5,cex.axis=1.5, col = 1:10)
+legend('topleft',legend="F SD = .01",bty='n', cex = 1.5)
+matplot(x=seq(1,100,1),t(Edat_var.05),type='l',ylim=c(0,0.2),xlab="",ylab="Fishing mortality", lwd = 3,cex.lab=1.5,cex.axis=1.5,col = 1:10)
+legend('topleft',legend="F SD = .05",bty='n', cex = 1.5)
+matplot(x=seq(1,100,1),t(Edat_var.1),type='l',ylim=c(0,0.2),xlab="",ylab="Fishing mortality", lwd = 3,cex.lab=1.5,cex.axis=1.5)
+legend('topleft',legend="F SD = .1",bty='n', cex = 1.5)
+
+Edat_all <- t(rbind(Edat[c(2,5,9),], Edat_var.01[c(1,2,6),], Edat_var.05[c(6,4,10),], Edat_var.1[c(6,1,9),]))
+matplot(x=seq(1,100,1),Edat_all,type='l',ylim=c(0,0.2),xlab="",ylab="Fishing mortality", lwd = 3,cex.lab=1.5,cex.axis=1.5)
+
+
+for(i in 5:ncol(Edat_all)){
+
+    new.dir <- file.path(main.dir, "ss3sim-example", paste0("cod-om-simF-", i+7))
+    dir.create(new.dir)
+    files_to_cp <- c("codOM.ctl", "data_new.ss", "forecast.ss", "starter.ss", "ss.exe")
+    file.copy(from = file.path(main.dir, "ss3sim-example", "cod-om", files_to_cp), 
+            to = new.dir, overwrite = T)
+
+    dat <- r4ss::SS_readdat(
+    file.path(new.dir, "data_new.ss"),
+    verbose = FALSE
+    )
+    ctl <- r4ss::SS_readctl(
+    file.path(new.dir, "codOM.ctl"),
+    verbose = FALSE, use_datlist = TRUE, datlist = dat
+    )
+
+    newctl <- change_f(years = 1:100, fleets = 1, fvals = Edat_all[,i], ctl_list = ctl)
+
+    SS_writectl_3.30(newctl, outfile = file.path(new.dir, "codOM.ctl"), overwrite = TRUE)
+
+    r4ss::run(dir = new.dir, exe = "ss", extras = "-nohess")
+    file.rename(from = file.path(new.dir, "data_expval.ss"), to = file.path(new.dir, "data_newF.ss"))
+    start <- SS_readstarter(file.path(new.dir, "starter.ss"))
+    start$datfile <- "data_newF.ss"
+    SS_writestarter(start, dir = new.dir, overwrite = T)
+    r4ss::run(dir = new.dir, exe = "ss", extras = "-nohess", skipfinished = F)
+
+}
+
+simFreps <- SSgetoutput(dirvec = paste0(file.path(main.dir, "ss3sim-example"), "/cod-om-simF-", c(2,5,9,11:19)))
+simFsum <- SSsummarize(simFreps)
+head(simFsum$indices)
+ind <- simFsum$indices |> select(imodel, Fleet, Yr, Obs) |> filter(Fleet == 1)
+head(ind)
+ind_ssb <- simFsum$SpawnBio |> 
+  filter(Yr > 0 & Yr <= 100) |> 
+  select(-Label) |> 
+  rename_with(~ paste0("imodel_", seq(1,12)), starts_with("replist")) |> 
+  pivot_longer(cols = -"Yr", names_to = "imodel", values_to = "SSB") |> 
+  separate(col = imodel, into = c("lab", "imodel"), sep = "_") |> 
+  select(-lab) |> mutate(imodel = as.integer(imodel)) |> 
+  left_join(ind, by = c("Yr", "imodel")) 
+
+sum.df <- Edat_all  |> 
+  as.data.frame() |> 
+  rename_with(~ paste0("imodel_", seq(1,12))) |> 
+  mutate(Yr = seq(1,100)) |> 
+  pivot_longer(cols = -"Yr", names_to = "imodel", values_to = "F_mortality") |> 
+  separate(col = imodel, into = c("lab", "imodel"), sep = "_") |> 
+  select(-lab) |> 
+  mutate(imodel = as.integer(imodel))  |>   
+  left_join(ind_ssb, by = c("Yr", "imodel"))  
+
+gg_fmort <- sum.df |> 
+filter(imodel >= 5) |> 
+mutate(imodel = as.factor(imodel)) |> 
+ggplot() +
+geom_line(aes(x = Yr, y = F_mortality, group = imodel, color = imodel), show.legend = F) +
+facet_wrap(~imodel, ncol = 1, scales = "free_y") +
+theme(text = element_text(size = 16)) +
+labs(y = "Fishing Mortality")
+
+gg_ind <-  sum.df |> 
+filter(imodel >= 5) |> 
+mutate(imodel = as.factor(imodel)) |> 
+ggplot() +
+geom_line(aes(x = Yr, y = Obs, group = imodel, color = imodel), show.legend = F) +
+facet_wrap(~imodel, ncol = 1, scales = "free_y") +
+theme(text = element_text(size = 16)) +
+labs(y = "Index of Abundance")
+
+gg_ssb <- sum.df |> 
+filter(imodel >= 5) |> 
+mutate(imodel = as.factor(imodel)) |> 
+ggplot() +
+geom_line(aes(x = Yr, y = SSB, group = imodel, color = imodel), show.legend = F) +
+facet_wrap(~imodel, ncol = 1, scales = "free_y") +
+theme(text = element_text(size = 16))
+
+#library(patchwork)
+gg_fmort + gg_ind + gg_ssb
---
+++ R/transition-cod-to-opaka.r
@@ -0,0 +1,356 @@
+library(r4ss)
+library(tidyverse)
+library(ss3sim)
+
+main.dir <- this.path::here(.. = 1)
+## Checking the original EM estimates the original OM well
+om_dir <- file.path(main.dir, "models", "cod-om")
+em_dir <- file.path(main.dir, "models", "cod-em")
+
+F0 <- list(
+  years = 1:100,
+  fleets = 1,
+  fvals = c(rep(0, 25), rep(0.114, 75))
+)
+
+index1 <- list(
+  fleets = c(2), years = list(seq(62, 100, by = 2)),
+  sds_obs = list(0.1)
+)
+
+lcomp1 <- list(
+  fleets = c(1, 2), Nsamp = list(50, 100),
+  years = list(26:100, seq(62, 100, by = 2))
+)
+
+agecomp1 <- list(
+  fleets = c(1, 2), Nsamp = list(50, 100),
+  years = list(26:100, seq(62, 100, by = 2))
+)
+
+##Function copies the OM and EM input files to new directory. Then is inserts dummy data for the amount of years you specifiy for each year and save that as ss3.dat. Then runs model to generate data_expval.ss (with init_scrc = 0) and recdevs and F values specified in ctl file. Then that data file is sent to EM directory. Each data point in EM/ss3.dat file is sampled from the OM/data_expval.ss with specified error. For example: if catch in year 50 = 4950 in OM (ie true catch), then in EM/ss3.dat, catch in year 50 = rnorm(mu = 4950, sd = .1). In control file, there are no recdevs or F specified (they are estimated).
+
+ss3sim_base(
+  iterations = 1,
+  scenarios = "D1-E0-F0-cod", # name as desired
+  f_params = F0,
+  index_params = index1,
+  lcomp_params = lcomp1,
+  agecomp_params = agecomp1,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+
+ex_mods <- SSgetoutput(dirvec = c(
+    file.path(main.dir, "D1-E0-F0-cod", "1", "om"),
+    file.path(main.dir, "D1-E0-F0-cod", "1", "em")
+))
+ex_mods_sum <- SSsummarize(ex_mods)
+SSplotComparisons(ex_mods_sum) ## EM matches very closely to OM and fits OM data tightly
+
+### Changing cod-om and cod-em to opaka specs #######
+# All changes were made manually by first matching the models/codOM.ctl or models/codOM.dat file to the No_size_comp/OM/control.ss_new or /data.ss files. Then models/codEM.ctl file was changed to match models/codOM.ctl. Changes were implemented step-wise and new directories were created at each step and OM/EM comparison plots were created to ensure they were still matching well.
+#change life history in cod-om and cod-em 
+ss3sim_base(
+  iterations = 1,
+  scenarios = "01-LH-trans-cod", 
+  f_params = F0,
+  index_params = index1,
+  lcomp_params = lcomp1,
+  agecomp_params = agecomp1,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+lh_mods <- SSgetoutput(dirvec = c(
+    file.path(main.dir, "01-LH-trans-cod", "1", "om"),
+    file.path(main.dir, "01-LH-trans-cod", "1", "em")
+))
+lh_mods_sum <- SSsummarize(lh_mods)
+SSplotComparisons(lh_mods_sum, print = TRUE, legendlabels = c("OM", "EM"), plotdir = file.path(main.dir, "01-LH-trans-cod", "1"))
+
+#change the stock recruitment parameters section in cod-om and cod-em 
+ss3sim_base(
+  iterations = 1,
+  scenarios = "02-SR-trans-cod", 
+  f_params = F0,
+  index_params = index1,
+  lcomp_params = lcomp1,
+  agecomp_params = agecomp1,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+sr_mods <- SSgetoutput(dirvec = c(
+    file.path(main.dir, "02-SR-trans-cod", "1", "om"),
+    file.path(main.dir, "02-SR-trans-cod", "1", "em")
+))
+sr_mods_sum <- SSsummarize(sr_mods)
+SSplotComparisons(sr_mods_sum, print = TRUE, legendlabels = c("OM", "EM"), plotdir = file.path(main.dir, "02-SR-trans-cod", "1"))
+
+#change the length selectivity of fishery and survey (use bfish camera first bc its asymptotic) and length bins of length comp data
+ss3sim_base(
+  iterations = 1,
+  scenarios = "03-Selex-trans-cod", 
+  f_params = F0,
+  index_params = index1,
+  lcomp_params = lcomp1,
+  agecomp_params = agecomp1,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+sx_mods <- SSgetoutput(dirvec = c(
+    file.path(main.dir, "03-Selex-trans-cod", "1", "om"),
+    file.path(main.dir, "03-Selex-trans-cod", "1", "em")
+))
+sx_mods_sum <- SSsummarize(sx_mods)
+SSplotComparisons(sx_mods_sum, print = TRUE, legendlabels = c("OM", "EM"), plotdir = file.path(main.dir, "03-Selex-trans-cod", "1"))
+
+#change growth platoon
+ss3sim_base(
+  iterations = 1,
+  scenarios = "04-GP-trans-cod", 
+  f_params = F0,
+  index_params = index1,
+  lcomp_params = lcomp1,
+  agecomp_params = agecomp1,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+gp_mods <- SSgetoutput(dirvec = c(
+    file.path(main.dir, "04-GP-trans-cod", "1", "om"),
+    file.path(main.dir, "04-GP-trans-cod", "1", "em")
+))
+gp_mods_sum <- SSsummarize(gp_mods)
+SSplotComparisons(gp_mods_sum, print = TRUE, legendlabels = c("OM", "EM"), plotdir = file.path(main.dir, "04-GP-trans-cod", "1"))
+
+#change age selectivity and age bins 
+ss3sim_base(
+  iterations = 1,
+  scenarios = "05-age-trans-cod", 
+  f_params = F0,
+  index_params = index1,
+  lcomp_params = lcomp1,
+  agecomp_params = agecomp1,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+age_mods <- SSgetoutput(dirvec = c(
+    file.path(main.dir, "05-age-trans-cod", "1", "om"),
+    file.path(main.dir, "05-age-trans-cod", "1", "em")
+))
+age_mods_sum <- SSsummarize(age_mods)
+SSplotComparisons(age_mods_sum, print = TRUE, legendlabels = c("OM", "EM"), plotdir = file.path(main.dir, "05-age-trans-cod", "1"))
+
+#add non-commerical fleet 
+F2 <- list(
+  years = list(1:75, 1:75),
+  fleets = c(1, 3), 
+  fvals = list(rep(0.114, 75), rep(0.114, 75))
+)
+
+index2 <- list(
+  fleets = 1:2, years = list(seq(1, 75, by = 1), seq(69, 75, by = 1)),
+  seas = list(7,1), sds_obs = list(0.1, 0.1)
+)
+
+lcomp2 <- list(
+  fleets = c(2), Nsamp = list(45),
+  years = list(seq(69, 75, by = 1))
+)
+
+agecomp2 <- list(
+  fleets = c(2), Nsamp = list(45),
+  years = list(seq(69, 75, by = 1))
+)
+
+ss3sim_base(
+  iterations = 1,
+  scenarios = "06-non-comm-trans-cod", 
+  f_params = F2,
+  index_params = index2,
+  lcomp_params = lcomp2,
+  agecomp_params = agecomp2,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+
+nc_mods <- SSgetoutput(dirvec = c(
+    file.path(main.dir, "06-non-comm-trans-cod", "1", "om"),
+    file.path(main.dir, "06-non-comm-trans-cod", "1", "em")
+))
+nc_mods_sum <- SSsummarize(nc_mods)
+SSplotComparisons(nc_mods_sum, print = TRUE, legendlabels = c("OM", "EM"), plotdir = file.path(main.dir, "06-non-comm-trans-cod", "1"))
+
+#add BFISH research fishing fleet
+F2 <- list(
+  years = list(1:75, 1:75),
+  fleets = c(1, 3), 
+  fvals = list(rep(0.114, 75), rep(0.114, 75))
+)
+
+index2 <- list(
+  fleets = 1:2, years = list(seq(1, 75, by = 1), seq(69, 75, by = 1)),
+  seas = list(7,1), sds_obs = list(0.1, 0.1)
+)
+
+lcomp2 <- list(
+  fleets = c(2), Nsamp = list(45),
+  years = list(seq(69, 75, by = 1))
+)
+
+agecomp2 <- list(
+  fleets = c(2), Nsamp = list(45),
+  years = list(seq(69, 75, by = 1))
+)
+
+ss3sim_base(
+  iterations = 1,
+  scenarios = "07-resfish-trans-cod", 
+  f_params = F2,
+  index_params = index2,
+  lcomp_params = lcomp2,
+  agecomp_params = agecomp2,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+bf_mods <- SSgetoutput(dirvec = c(
+    file.path(main.dir, "07-resfish-trans-cod", "1", "om"),
+    file.path(main.dir, "07-resfish-trans-cod", "1", "em")
+))
+bf_mods_sum <- SSsummarize(bf_mods)
+SSplotComparisons(bf_mods_sum, print = TRUE, legendlabels = c("OM", "EM"), plotdir = file.path(main.dir, "07-resfish-trans-cod", "1"))
+
+## Put in F timeseries from original SS model and see how it behaves
+rep <- SS_output(dir = file.path(main.dir, "models", "03-modified-og"))
+head(rep$timeseries)
+head(rep$exploitation)
+
+F_frs <- rep$exploitation$FRS
+F_noncomm <- rep$exploitation$Non_comm
+
+F3 <- list(
+  years = list(1:75, 1:75),
+  fleets = c(1, 3), 
+  fvals = list(F_frs, F_noncomm)
+)
+
+index3 <- list(
+  fleets = c(1,2,4), years = list(seq(1, 75, by = 1), seq(69, 75, by = 1), seq(69, 75, by =1)),
+  seas = list(7,1,1), sds_obs = list(0.25, 0.25, 0.25)
+)
+
+lcomp3 <- list(
+  fleets = c(2,4), Nsamp = list(45,45),
+  years = list(seq(69, 75, by = 1), seq(69, 75, by = 1))
+)
+
+agecomp3 <- list(
+  fleets = c(2,4), Nsamp = list(45,45),
+  years = list(seq(69, 75, by = 1), seq(69, 75, by = 1))
+)
+
+ss3sim_base(
+  iterations = 1,
+  scenarios = "08-trueF-trans-cod", 
+  f_params = F3,
+  index_params = index3,
+  lcomp_params = lcomp3,
+  agecomp_params = agecomp3,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+
+## Modify original models to match the years in ss3sim model (instead of 1949-2023, 1-75)
+data_orig <- SS_readdat_3.30(file = file.path(main.dir, "models", "03-modified-og", "data.ss"))
+data_orig$styr <- 1
+data_orig$endyr <- 75
+data_orig$catch <- data_orig$catch  |> mutate(year = ifelse(year == -999, year, year - 1948))
+data_orig$CPUE$year <- data_orig$CPUE$year - 1948
+data_orig$lencomp$Yr <- data_orig$lencomp$Yr - 1948
+#data_orig$agecomp$Yr <- data_orig$agecomp$Yr - 1948
+#data_orig$sizefreq_data_list[[1]]$Yr <- data_orig$sizefreq_data_list[[1]]$Yr - 1948
+SS_writedat_3.30(data_orig, outfile = file.path(main.dir, "models", "03-modified-og", "data_updated.ss"))
+ctl <- SS_readctl_3.30(file = file.path(main.dir, "models", "03-modified-og", "control.ss"), use_datlist = T, datlist = file.path(main.dir, "models", "03-modified-og", "data_updated.ss"))
+ctl$last_early_yr_nobias_adj <- 1
+ctl$first_yr_fullbias_adj <- 26
+ctl$last_yr_fullbias_adj <- 74
+ctl$first_recent_yr_nobias_adj <- 75
+ctl$MainRdevYrFirst <- 1
+ctl$MainRdevYrLast <- 75
+SS_writectl_3.30(ctl, outfile = file.path(main.dir, "models", "03-modified-og", "control.ss"), overwrite = TRUE)
+start <- SS_readstarter(file = file.path(main.dir, "models", "03-modified-og", "starter.ss"))
+start$datfile <- "data_updated.ss"
+SS_writestarter(start, file.path(main.dir, "models", "03-modified-og", "starter.ss"), overwrite = TRUE)
+
+trueF_mods <- SSgetoutput(dirvec = c(
+    file.path(main.dir, "08-trueF-trans-cod", "1", "om"),
+    file.path(main.dir, "08-trueF-trans-cod", "1", "em"),
+    file.path(main.dir, "models", "02-no-size-comp")
+))
+trueF_mods_sum <- SSsummarize(trueF_mods)
+SSplotComparisons(trueF_mods_sum, print = TRUE, legendlabels = c("OM", "EM", "Original"), plotdir = file.path(main.dir, "08-trueF-trans-cod", "1"))
+# NOTE: 4/22 Model comparison looks pretty good but SSB scale is different
+
+ss3sim_base(
+  iterations = 1:5,
+  scenarios = "09-recdevs_ageselex-trans-cod", 
+  f_params = F3,
+  index_params = index3,
+  lcomp_params = lcomp3,
+  agecomp_params = agecomp3,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+
+recdevs_mods <- SSgetoutput(dirvec = c(
+    #file.path(main.dir, "09-recdevs_ageselex-trans-cod", "1", "om"),
+    #file.path(main.dir, "09-recdevs_ageselex-trans-cod", "1", "em"),
+    file.path(main.dir, "09-recdevs_ageselex-trans-cod", "2", "em"),
+    #file.path(main.dir, "09-recdevs_ageselex-trans-cod", "3", "em"),
+    #file.path(main.dir, "09-recdevs_ageselex-trans-cod", "4", "em"),
+    file.path(main.dir, "09-recdevs_ageselex-trans-cod", "5", "em"),
+    file.path(main.dir, "models", "03-modified-og")
+))
+recdevs_mods_sum <- SSsummarize(recdevs_mods)
+SSplotComparisons(recdevs_mods_sum, legendlabels = c("Rep_2", "Rep_5", "Original_nosize")) #, print = TRUE, plotdir = file.path(main.dir, "08-trueF-trans-cod", "1"))
+
+
+## Side work: Creating an EM for FIMS case study
+#only using 2 fleets, FRS and BFISH camera for now, need catch, cpue, and age comps
+F_frs <- rep$exploitation$FRS
+
+F_fims <- list(
+  years = list(1:75),
+  fleets = c(1), 
+  fvals = list(F_frs)
+)
+
+index_fims <- list(
+  fleets = c(1,2), years = list(seq(1, 75, by = 1), seq(69, 75, by = 1)),
+  seas = list(7,1), sds_obs = list(0.1, 0.25)
+)
+
+lcomp_fims <- list(
+  fleets = c(2), Nsamp = list(45),
+  years = list(seq(69, 75, by = 1))
+)
+
+agecomp_fims <- list(
+  fleets = c(1,2), Nsamp = list(100,45),
+  years = list(seq(26, 75, by = 1), seq(69, 75, by = 1))
+)
+
+ss3sim_base(
+  iterations = 1,
+  scenarios = "FIMS-em", 
+  f_params = F_fims,
+  index_params = index_fims,
+  lcomp_params = lcomp_fims,
+  agecomp_params = agecomp_fims,
+  om_dir = om_dir,
+  em_dir = em_dir
+)
+
+
+rep <- SS_output(file.path(main.dir, "models", "03-modified-og"))
+rep <- SS_output(file.path(main.dir, "09-recdevs_ageselex-trans-cod", "1", "om"))
+SS_plots(rep)
