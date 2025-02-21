5cefbbd1e961d8ca268fb808b1699431ca1bb84a
MOshima-PIFSC <megumi.oshima@noaa.gov>
2025-02-20 14:30:07 -1000

    for selex scenarios

--- osg/OSG_om_em.r
+++ osg/OSG_om_em.r
@@ -20,11 +20,11 @@ load("recdevs_mat.RData")
 #Variables
 nyears <- 100
 nyears_fwd <- 25
-scen <- "IRF_SQ"
+scen <- "IRF_Selex"
 
 #Template OM and EM files
-om_dir <- paste0("opaka-om-", nyears_fwd, "/")
-em_dir <- paste0("opaka-em-", nyears_fwd, "/")
+om_dir <- paste0("opaka-om-", nyears_fwd, "-selex/")
+em_dir <- paste0("opaka-em-", nyears_fwd, "-selex/")
 
 #Get iteration number
 I <- as.numeric(tail(strsplit(args[1], "/")[[1]], n = 1))
@@ -65,34 +65,3 @@ ss3sim_base(
     bias_adjust = T,
     seed = seed
 )
-
- if(str_detect(scen, "hyperstable")){
-
-        scen_prefix <- strsplit(scen, "hyperstable")[[1]][1]
-        regular_em <- SS_readdat_3.30(file.path(main.dir, paste(scen_prefix, nyears_fwd, "yrfwd", sep = "_"), I, "em", "ss3.dat"))
-        em_path <- file.path(main.dir, paste(scen, nyears_fwd, "yrfwd", sep = "_"), I, "em")
-        #replace catch and CPUE for commercial fishery
-        em_dat <- SS_readdat_3.30(file = file.path(em_path, "ss3.dat"))
-        em_dat$catch <- regular_em$catch
-        em_dat$CPUE$obs[1:nyears] <- regular_em$CPUE$obs[1:nyears]
-        SS_writedat(em_dat, file.path(em_path, "ss3.dat"), overwrite = T)
-        r4ss::run(dir = em_path, exe = "ss3", skipfinished = F, verbose = F)
-        
-        #clean for next run
-        rm(list = c("om_dat", "em_dat"))
-
-    }
-
-#list.dirs(getwd())    
-#print(paste("The current directory is ", getwd()))
-
-# extract model output
-#ss_report_om <- SS_output(dir=file.path(getwd(), paste(scen, nyears_fwd, "yrfwd", sep = "_"), I, "om"))
-#ss_report_em <- SS_output(dir=file.path(getwd(), paste(scen, nyears_fwd, "yrfwd", sep = "_"), I, "em"))
-# make sure report files were read successfully
-#exists("ss_report_om")
-#exists("ss_report_em")
-
-# save output
-#save(list = "ss_report_om",file="ss_report_om.RData")
-#save(list = "ss_report_em", file = "ss_report_em.RData")
